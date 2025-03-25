5c528507a04f9cd8fc27aaab6cb3bfb80a7c47aa
FabioMorrone <morronefabio04@gmail.com>
2025-03-25 16:51:27 +0100

    add linguaggi data module with programming languages

---
+++ src/data/linguaggi.js
@@ -0,0 +1,34 @@
+const Linguaggio = [
+    {
+        id: 1,
+        nome: "Html",
+        descriozione: "lorem ipsum",
+    },
+    {
+        id: 2,
+        nome: "Css",
+        descrizione: "lorem ipsum",
+    },
+    {
+        id: 3,
+        nome: "Javascript",
+        descrizione: "lorem ipsum",
+    },
+    {
+        id: 4,
+        nome: "Node.js",
+        descrizione: "lorem ipsum",
+    },
+    {
+        id: 5,
+        nome: "Express",
+        descrizione: "lorem ipsum",
+    },
+    {
+        id: 6,
+        nome: "React",
+        descrizione: "lorem ipsum",
+    },
+]
+
+export default Linguaggio;
